1672d92e5ab4897e82bbc7aa9578e68ea590e0ea
AartiRawat01 <122513928+AartiRawat01@users.noreply.github.com>
2023-01-18 22:58:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
+.btn{
+	color:black;
+	background-color:yellow;
+	border-radius:30px;
+}
+
 
 .btn:hover{
 	color:white;
 	background-color:black;
 	border-radius:5px;
-}
-.btn{
-	color:black;
-	background-color:yellow;
-	border-radius:30px;
 }
